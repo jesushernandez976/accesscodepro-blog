9b66fd7896f3d2a58f8341e8d2a32314b5829847
jesushernandez976 <jp9000xx@outlook.com>
2025-07-19 21:34:59 -0500

    updates 5

--- backend/index.js
+++ backend/index.js
@@ -27,14 +27,7 @@ app.use(clerkMiddleware());
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
