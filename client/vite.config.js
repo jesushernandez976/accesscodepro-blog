097f1bf371df4d1af9298a9fa9581f2ac84aaa78
jesushernandez976 <jp9000xx@outlook.com>
2025-07-21 00:57:06 -0500

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    assetsInlineLimit: 0, // Prevents font inlining
+  },
+  assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf'],
 
   server: {
     host: true,
   },
+
+  
 })
 
